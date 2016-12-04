31e807f52a8793041057a6eb93fc3a9ad152fc44
Sergei Zinin <zinin@xakep.ru>
2016-12-05 01:59:28 +0700

    Lol

--- lib/petri.rb
+++ lib/petri.rb
@@ -1,13 +1,13 @@
 require 'petri/element'
 require 'petri/node'
 require 'petri/arc'
-require 'petri/message'
 require 'petri/net'
 require 'petri/net_loader'
 require 'petri/place'
 require 'petri/subprocess'
 require 'petri/token'
 require 'petri/transition'
+require 'petri/message'
 require 'petri/task_transition'
 
 module Petri
