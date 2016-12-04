5bb99d5c4d2a945a73c0e91377d2761b153af9b1
Sergei Zinin <zinin@xakep.ru>
2016-12-05 01:57:21 +0700

    New release again

--- lib/petri.rb
+++ lib/petri.rb
@@ -1,14 +1,14 @@
-require 'petri/arc'
 require 'petri/element'
+require 'petri/node'
+require 'petri/arc'
 require 'petri/message'
 require 'petri/net'
 require 'petri/net_loader'
-require 'petri/node'
 require 'petri/place'
 require 'petri/subprocess'
-require 'petri/task_transition'
 require 'petri/token'
 require 'petri/transition'
+require 'petri/task_transition'
 
 module Petri
 end
