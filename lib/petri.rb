d21b7692771859dfc4a85587d2086c6c8cf67f04
Sergei Zinin <zinin@xakep.ru>
2017-01-25 16:25:27 +0700

    Moar files to include

--- lib/petri.rb
+++ lib/petri.rb
@@ -1,7 +1,11 @@
 require 'petri/element'
 require 'petri/node'
 require 'petri/arc'
+require 'petri/net_loader/arcs'
+require 'petri/net_loader/bpf_reader'
+require 'petri/net_loader/tasks'
 require 'petri/net_loader'
+require 'petri/net/messages'
 require 'petri/net'
 require 'petri/place'
 require 'petri/subprocess'
@@ -9,6 +13,7 @@ require 'petri/token'
 require 'petri/transition'
 require 'petri/message'
 require 'petri/task_transition'
+require 'petri/task'
 
 module Petri
 end
